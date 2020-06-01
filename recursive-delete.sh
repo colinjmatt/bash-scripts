707c7b7e251675917690e20c76b849e422299e73
Colin Matthews <colin@matthews-co.uk>
2020-06-01 14:59:13 +0100

    Create recursive-delete.sh

---
+++ recursive-delete.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+for parentfolder in *; do
+  cd "$parentfolder" || exit
+
+  for folder in *; do
+    cd "$folder" || exit
+
+    for filename in *; do
+      if [[ -f "$filename" ]]; then
+        base=${filename%.*}
+        ext=${filename#$base.}
+        mkdir -p "${ext}"
+        mv "$filename" "${ext}"
+      fi
+    done
+    cd ..
+
+  done
+  cd ..
+  rm -rf POKES csw dck fdi ipf mdr mgt opd rom scl slt sp szx trd udi
+done
