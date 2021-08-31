72edbbb814ccbf0536273add26ccff44d97defa9
Colin Matthews <colin@Colin-MacBook-Pro.local>
2021-08-31 13:50:01 +0100

    Update freespace-alert.sh

--- freespace-alert.sh
+++ freespace-alert.sh
@@ -5,7 +5,10 @@ do
   usep=$(echo "$output" | awk '{ print $1}' | cut -d'%' -f1  )
   partition=$(echo "$output" | awk '{ print $2 }' )
   if [ "$usep" -ge 90 ]; then
-    echo "Space is low on \"$partition ($usep%)\" on $(hostname) as on $(date)" |
-    mail -s "Alert: Disk usage is above 90% ($usep%)" root@localhost # Change to the email address this alert should go to
+    echo "Space is low on \"$partition ($usep%)\" on $(hostname) as on $(date)" | \
+    mail \
+      -s "Alert: Disk usage is above 90% ($usep%)" \
+      -r root@example.com \
+    root@example.com # Change to the email address this alert should go to
   fi
 done
