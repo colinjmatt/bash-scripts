5af11345f450c984cfe8efc9e9196999566ff93b
colinjmatt <colinjmatt@users.noreply.github.com>
2025-03-30 13:26:24 +0100

    Update shitty-mac-files.sh

--- shitty-mac-files.sh
+++ shitty-mac-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find /mnt/* \(  -name ".DS_Store" \
-                      -or -name ".TemporaryItems" \
-                      -or -name ".Trashes" \
-                      -or -name "._*" \
-                      -or -name ".apdisk" \) \
-                      -exec rm -r "{}" \;
+                -or -name ".TemporaryItems" \
+                -or -name ".Trashes" \
+                -or -name "._*" \
+                -or -name ".apdisk" \) \
+                -exec rm -r "{}" \;
